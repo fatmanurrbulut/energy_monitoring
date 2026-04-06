1370a2b6704a3ef237da67c0717c678d13842313
Fatmanur Bulut <fatmanurrbulut@gmail.com>
2026-04-06 15:59:50 +0300

    Refactor password generation script to check for existing passwd file before creating

--- mosquitto/config/generate-passwd.sh
+++ mosquitto/config/generate-passwd.sh
@@ -13,7 +13,13 @@ CONFIG_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 docker run --rm \
   -v "${CONFIG_DIR}:/work" \
   eclipse-mosquitto:2 \
-  sh -lc "mosquitto_passwd -b -c /work/passwd '${USERNAME}' '${PASSWORD}'"
+  sh -lc "
+    if [ -f /work/passwd ]; then
+      mosquitto_passwd -b /work/passwd '${USERNAME}' '${PASSWORD}'
+    else
+      mosquitto_passwd -b -c /work/passwd '${USERNAME}' '${PASSWORD}'
+    fi
+  "
 
 chmod 600 "${CONFIG_DIR}/passwd"
 echo "Generated ${CONFIG_DIR}/passwd for user ${USERNAME}"
